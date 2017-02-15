75a3ccb38313e43f62ce152c723e66b329db5dae
jeffr <jeffr@DESKTOP-RH3DS7R>
2017-02-15 05:41:27 -0500

    adding test calendar api controller

---
+++ IshServices/Controllers/CalendarController.cs
@@ -0,0 +1,61 @@
+﻿using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using IshServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace IshServices.Controllers
+{
+    public class CalendarController : ApiController
+    {
+
+        private static string _calendarID = ConfigurationManager.AppSettings["CalendarID"];
+        private static string _apiKey = ConfigurationManager.AppSettings["CalendarApiKey"];
+        // GET api/<controller>
+        public IEnumerable<CalendarEvent> Get()
+        {
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                ApiKey = _apiKey
+            });
+
+            EventsResource.ListRequest request = service.Events.List(_calendarID);
+            request.TimeMin = new DateTime(2017, 2, 1);
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.MaxResults = 4;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+            Events events = request.Execute();
+
+            if (events.Items == null)
+            {
+                return null;
+            }
+
+            return events.Items.Select(evt =>
+                new CalendarEvent()
+                {
+                    Title = evt.Summary,
+                    Description = evt.Description,
+                    StartTime = evt.Start.DateTime,
+                    EndTime = evt.End.DateTime
+                }
+            );
+
+        }
+
+        // GET api/<controller>/5
+        public CalendarEvent Get(string id)
+        {
+            return new CalendarEvent();
+        }
+
+    }
+}
---
+++ IshServices/Models/CalendarEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IshServices.Models
+{
+    public class CalendarEvent
+    {
+        public string Title { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public string Description { get; set; }
+
+    }
+}
