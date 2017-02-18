8dcb2c58b8412158d2491eb6bd868fb508a26c47
jeffr <jeffr@DESKTOP-RH3DS7R>
2017-02-18 06:30:50 -0500

    adding caching to service call to get events

---
+++ IshServices/Models/ExpiringCache.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IshServices.Models
+{
+    public class ExpiringCache<T>
+    {
+        
+        public ExpiringCache(T item, TimeSpan cacheDuration)
+        {
+            Item = item;
+            _cacheCreated = DateTime.Now;
+            CacheDuration = cacheDuration;
+        }
+
+        public TimeSpan CacheDuration { get; private set; }
+        private DateTime _cacheCreated;
+
+        public T Item { get; private set; }
+
+        public bool IsExpired()
+        {
+            return DateTime.Now.Subtract(_cacheCreated).TotalMilliseconds > CacheDuration.TotalMilliseconds;
+        }
+
+        public void Refresh(T item)
+        {
+            Item = item;
+            _cacheCreated = DateTime.Now;
+        }
+    }
+}
